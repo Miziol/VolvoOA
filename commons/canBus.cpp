4926dc161f12e20494ba1013af6f57d8458a03b9
kacper.miziolek <kacper.miziolek@freymedical.eu>
2024-01-25 21:41:51 +0100

    test functions

--- commons/canBus.cpp
+++ commons/canBus.cpp
@@ -33,13 +33,29 @@ bool CanBus::getOilTemp(int *temp) {
     return readTemp(temp);
 }
 
+void CanBus::clearDIM() {
+    unsigned char clear[8] = {0xE1, 0xFE, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    can.sendMsgBuf(DIM_MESSAGE_ID, 0, 8, clear);
+}
+
+void CanBus::sendTestMessage() {
+    unsigned char part1[8] = {0xA7, 0x00, 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20}; // "Hello "
+    unsigned char part2[8] = {0x21, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x20, 0x20}; // "World  "
+    unsigned char part3[8] = {0x22, 0x20, 0x20, 0x20, 0x56, 0x65, 0x72, 0x73}; // "   vers"
+    unsigned char part4[8] = {0x67, 0x69, 0x6F, 0x6E, 0x20, 0x30, 0x2E, 0x31}; // "ion 0.1"
+    can.sendMsgBuf(DIM_MESSAGE_ID, 0, 8, part1);
+    can.sendMsgBuf(DIM_MESSAGE_ID, 0, 8, part2);
+    can.sendMsgBuf(DIM_MESSAGE_ID, 0, 8, part3);
+    can.sendMsgBuf(DIM_MESSAGE_ID, 0, 8, part4);
+}
+
 void CanBus::requestObdPid(unsigned char pid) {
     unsigned char tmp[8] = {0x02, 0x01, pid, 0, 0, 0, 0, 0};
     can.sendMsgBuf(CAN_ID_PID, 0, 8, tmp);
 }
 
 bool CanBus::readTemp(int *temp) {
-    unsigned long timeout = millis();
+    unsigned long timeut = millis();
     unsigned char len = 0;
     unsigned char buf[8];
 
--- commons/canBus.h
+++ commons/canBus.h
@@ -4,6 +4,8 @@
 #define PID_COOLANT_TEMP    0x05
 #define PID_OIL_TEMP        0x5C
 
+#define DIM_MESSAGE_ID      0x00400008
+
 class CanBus {
     I2C_CAN can;
 
@@ -14,6 +16,10 @@ public:
 
     bool getCoolantTemp(int *temp);
     bool getOilTemp(int *temp);
+
+    void clearDIM();
+    void sendTestMessage();
+    // void sendMessageDIM(string firstLine, string secondLine);
 
 private:
     void requestObdPid(unsigned char pid);
