41f7992387ae77e086314b2066ba57199b8f9c1c
kacper.miziolek <kacper.miziolek@freymedical.corp>
2025-11-15 11:51:31 +0100

    canbus 0.1

--- autoapp/src/backend/car/longanI2CCanBusDevice.h
+++ autoapp/src/backend/car/longanI2CCanBusDevice.h
@@ -19,12 +19,20 @@ private:
 protected:
     bool open() override;
     void close() override;
+    void setConfigurationParameter(ConfigurationKey key, const QVariant & value) override;
 
     bool writeFrame(const QCanBusFrame &frame) override;
     QString interpretErrorFrame(const QCanBusFrame &errorFrame) override;
 
 private:
     void readLoop();
+
+    bool setRegister(unsigned char reg, QByteArray data);
+    void setSpeed(const QVariant &speed);
+
+    int mapSpeedToDefine(const QVariant &speed);
+    unsigned char makeCheckSum(QByteArray frame);
+
 };
 
 #endif  // ANDROID_AUTO_LONGANI2CCANBUSDEVICE_H
