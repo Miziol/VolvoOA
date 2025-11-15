c1e6bf14c57b4c473bf4deedb3f42d535154b577
kacper.miziolek <kacper.miziolek@freymedical.corp>
2025-11-15 11:51:31 +0100

    canbus 0.1

--- autoapp/src/backend/car/longanI2CCanBusDevice.cpp
+++ autoapp/src/backend/car/longanI2CCanBusDevice.cpp
@@ -8,6 +8,8 @@
 #include <QtConcurrent/QtConcurrent>
 #include <iostream>
 
+#include "../../external/LonganLabs I2C CAN/I2C_CAN_dfs.h"
+
 LonganI2CCanBusDevice::LonganI2CCanBusDevice(QObject *parent) : QCanBusDevice(parent) {}
 
 LonganI2CCanBusDevice::~LonganI2CCanBusDevice() {
@@ -48,19 +50,46 @@ void LonganI2CCanBusDevice::close() {
     }
 }
 
+void LonganI2CCanBusDevice::setConfigurationParameter(ConfigurationKey key, const QVariant &value) {
+    switch (key) {
+        case ConfigurationKey::RawFilterKey:
+            break;
+        case ConfigurationKey::ErrorFilterKey:
+            break;
+        case ConfigurationKey::LoopbackKey:
+            break;
+        case ConfigurationKey::ReceiveOwnKey:
+            break;
+        case ConfigurationKey::BitRateKey:
+            setSpeed(value);
+            break;
+        case ConfigurationKey::CanFdKey:
+            break;
+        case ConfigurationKey::DataBitRateKey:
+            break;
+        case ConfigurationKey::ProtocolKey:
+            break;
+        case ConfigurationKey::UserKey:
+            break;
+        default:
+            break;
+    }
+}
+
 bool LonganI2CCanBusDevice::writeFrame(const QCanBusFrame &frame) {
     if (state() != ConnectedState)
         return false;
 
-    QByteArray payload = frame.payload();
-    uint8_t buffer[16] = {0};
-    buffer[0] = (frame.frameId() >> 8) & 0xFF;
-    buffer[1] = frame.frameId() & 0xFF;
-    buffer[2] = payload.size();
-    memcpy(&buffer[3], payload.constData(), payload.size());
-
-    ::write(deviceFileDescriptor, buffer, 3 + payload.size());
-    return true;
+    QByteArray data;
+    for (int i = 3; i >= 0; i--) {
+        data.append(0xFF & (frame.frameId() >> (i * 8)));
+    }
+    data.append(frame.hasExtendedFrameFormat());
+    data.append(frame.frameType() == QCanBusFrame::RemoteRequestFrame);
+    data.append(frame.payload().size());
+    data.append(frame.payload());
+    data.append(makeCheckSum(data));
+    return setRegister(REG_SEND, data.constData());
 }
 
 QString LonganI2CCanBusDevice::interpretErrorFrame(const QCanBusFrame &frame) {
@@ -79,4 +108,73 @@ void LonganI2CCanBusDevice::readLoop() {
             enqueueReceivedFrames({frame});
         }
     }
+}
+
+bool LonganI2CCanBusDevice::setRegister(unsigned char reg, QByteArray data) {
+    data.prepend(reg);
+    int transferedBytes = ::write(deviceFileDescriptor, data.constData(), data.size());
+
+    return transferedBytes == data.size();
+}
+
+void LonganI2CCanBusDevice::setSpeed(const QVariant &speed) {
+    setRegister(REG_BAUD, QByteArray().append(mapSpeedToDefine(speed)));
+}
+
+int LonganI2CCanBusDevice::mapSpeedToDefine(const QVariant &speed) {
+    switch (speed.toInt()) {
+        case 5000:
+            return CAN_5KBPS;
+        case 10000:
+            return CAN_10KBPS;
+        case 20000:
+            return CAN_20KBPS;
+        case 25000:
+            return CAN_25KBPS;
+        case 31200:  // from docs
+        case 31250:  // from #define name
+            return CAN_31K25BPS;
+        case 33000:
+            return CAN_33KBPS;
+        case 40000:
+            return CAN_40KBPS;
+        case 50000:
+            return CAN_50KBPS;
+        case 80000:
+            return CAN_80KBPS;
+        case 83300:
+            return CAN_83K3BPS;
+        case 95000:
+            return CAN_95KBPS;
+        case 100000:
+            return CAN_100KBPS;
+        case 125000:
+            return CAN_125KBPS;
+        case 200000:
+            return CAN_200KBPS;
+        case 250000:
+            return CAN_250KBPS;
+        case 500000:
+            return CAN_500KBPS;
+        case 666000:
+            return CAN_666KBPS;
+        case 1000000:
+            return CAN_1000KBPS;
+        default:
+            return 0;
+    }
+}
+
+unsigned char LonganI2CCanBusDevice::makeCheckSum(QByteArray data) {
+    unsigned long sum = 0;
+    for (const auto &byte : data) {
+        sum += byte;
+    }
+
+    if (sum > 0xFF) {
+        sum = ~sum;
+        sum += 1;
+    }
+
+    return sum & 0xFF;
 }
