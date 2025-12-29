ada3508698f831bbd3a8fec5d319a7ceaff4afb9
Kacper <kacper.miziolek@freymedical.eu>
2025-12-29 22:30:37 +0100

    requirements

---
+++ scripts/install_requirements.sh
@@ -0,0 +1,6 @@
+sudo apt install -y \
+protobuf-compiler libprotobuf-dev libboost-system-dev libboost-log-dev libusb-1.0-0-dev \
+qt6-base-dev qt6-multimedia-dev qt6-declarative-dev qt6-connectivity-dev qt6-serialbus-dev \
+qml6-module-qtquick-controls qml6-module-qtquick qml6-module-qtqml-workerscript qml6-module-qtquick-templates qml6-module-qtquick-layouts qml6-module-qtmultimedia
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+arduino-cli lib install Streaming Vector
