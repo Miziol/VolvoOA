c70c102193cb5388995e8e219aa695b6439f5772
Kacper <kacper.miziolek@freymedical.eu>
2026-01-16 14:40:08 +0100

    fix requirements

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,5 @@
 sudo apt install -y \
-g++ cmake make
+g++ cmake make \
 protobuf-compiler libprotobuf-dev libboost-system-dev libboost-log-dev libusb-1.0-0-dev \
 qt6-base-dev qt6-multimedia-dev qt6-declarative-dev qt6-connectivity-dev qt6-serialbus-dev qt6-wayland \
 qml6-module-qtquick-controls qml6-module-qtquick qml6-module-qtqml-workerscript qml6-module-qtquick-templates qml6-module-qtquick-layouts qml6-module-qtmultimedia qml6-module-qtquick-window
